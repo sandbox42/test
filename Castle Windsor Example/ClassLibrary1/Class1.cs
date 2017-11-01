a401dce89251b48151daaa85bb0da3ad19e89030
sandbox42 <32909749+sandbox42@users.noreply.github.com>
2017-11-01 11:00:16 +0000

    initial commit

---
+++ Castle Windsor Example/ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public interface IDependency1
+    {
+        object SomeObject { get; set; }
+    }
+
+    public interface IDependency2
+    {
+        object SomeObject { get; set; }
+    }
+
+    public class Dependency1
+    {
+        public object SomeObject { get; set; }
+    }
+       
+    public class Dependency2
+    {
+        public object SomeObject { get; set; }
+    }
+
+    public class Main
+    { }
+}
